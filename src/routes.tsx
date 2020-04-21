a2f17edf6aaa545668b0646e28540b21a9a01266
raymondhalim <raymondhalimdotcom@gmail.com>
2020-04-21 19:16:59 +0800

    Generate sitemap.

---
+++ src/routes.tsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { BrowserRouter, Route, Switch } from "react-router-dom";
+import App from "./App";
+
+const Routes = () => {
+  return (
+    <BrowserRouter>
+      <Switch>
+        <Route exact path="/" component={App} />
+      </Switch>
+    </BrowserRouter>
+  );
+};
+
+export default Routes;
