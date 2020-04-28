e41602663f595e78a72b6edd74ad7976d39951f0
raymondhalim <raymondhalimdotcom@gmail.com>
2020-04-28 18:53:19 +0800

    Update cases data.

--- src/App.tsx
+++ src/App.tsx
@@ -30,7 +30,7 @@ import html2canvas from "html2canvas";
 const App: React.FC = () => {
   const residenceMapRef: any = useRef();
   const matches = useMediaQuery(theme.breakpoints.down("xs"));
-  const date = "27-Apr-2020";
+  const date = "28-Apr-2020";
   const [data, setData] = useState<PHCase[]>();
 
   useEffect(() => {
