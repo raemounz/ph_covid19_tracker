932f0d8a8493242cf1166d84bf1cc22a8cb46948
raymondhalim <raymondhalimdotcom@gmail.com>
2020-08-06 09:21:31 +0800

    Update cases data.

--- src/App.tsx
+++ src/App.tsx
@@ -22,7 +22,7 @@ import html2canvas from "html2canvas";
 import MainSummary from "./summary/MainSummary";
 
 const App: React.FC = () => {
-  const date = "2020-07-31";
+  const date = "2020-08-05";
   const [data, setData] = useState<PHCase[]>();
 
   useEffect(() => {
