9898a7fcab8a7038c699a326c1f6052f19b015db
raymondhalim <raymondhalimdotcom@gmail.com>
2020-08-07 22:47:25 +0800

    Force to use https.

--- server/server.js
+++ server/server.js
@@ -5,6 +5,13 @@ const port = process.env.PORT || 3000;
 const publicPath = path.join(__dirname, "..", "build");
 
 app.use(express.static(publicPath));
+app.use((req, res, next) => {
+  if (req.header('x-forwarded-proto') !== 'https') {
+    res.redirect(`https://${req.header('host')}${req.url}`)
+  } else {
+    next();
+  }
+});
 
 app.get("*", (req, res) => {
   res.sendFile(path.join(publicPath, "index.html"));
