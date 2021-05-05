cfa67d17077f40ba8c01af5ae52d125681486aa8
raymondhalim <raymondhalimdotcom@gmail.com>
2021-05-05 08:36:25 +0800

    Update cases data.

--- src/utility/CasesUtility.ts
+++ src/utility/CasesUtility.ts
@@ -6,7 +6,7 @@ const csv = require("csv");
 const fs = require("fs");
 
 function processData() {
-  fs.createReadStream("/Users/raymondhalim/Downloads/DOH COVID Data Drop_ 20210503 - 04 Case Information.csv")
+  fs.createReadStream("/Users/raymondhalim/Downloads/DOH COVID Data Drop_ 20210504 - 04 Case Information.csv")
     .pipe(csv.parse({ columns: true }))
     .pipe(
       csv.transform((input) => {
